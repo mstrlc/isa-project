caa065f7bf508c2592a3d2e76cf5892eaa149adb
Matyáš Strelec <matyasstrelec@gmail.com>
2023-11-16 21:08:15 +0100

    add BER encoding functions and update server to handle bindRequest

--- src/ber.cpp
+++ src/ber.cpp
@@ -1,36 +1,63 @@
 #include "ber.h"
 
-#include <arpa/inet.h>
-#include <unistd.h>
-
-#include <cstring>
 #include <iomanip>
-#include <string>
+#include <iostream>
 
-BER processTLV(unsigned char *data) {
-    BER ber;
-    ber.tag = data[0];
-    ber.length = data[1];
-    ber.value = data + 2;
-
-    return ber;
-}
-
-void processData(unsigned char *data) {
-    int len = data[1];
-    std::cout << "Length: " << len << std::endl;
-
-    BER outermost = processTLV(data);
-    // print BER
-    std::cout << std::endl;
-    std::cout << "Outermost BER: " << std::endl;
-    std::cout << "Tag: 0x" << std::setw(2) << std::setfill('0') << std::hex << int(outermost.tag) << std::endl;
-    std::cout << "Length: 0x" << std::setw(2) << std::setfill('0') << std::hex << int(outermost.length) << std::endl;
-    std::cout << "Value: " << std::endl;
-
-    for (int i = 0; i < int(outermost.length); i++) {
-        std::cout << "0x" << std::setw(2) << std::setfill('0') << std::hex << int(outermost.value[i]) << " ";
+void print_hex(std::vector<unsigned char> data) {
+    for (size_t i = 0; i < data.size(); i++) {
+        std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << (int)data[i] << " ";
     }
-
     std::cout << std::endl;
 }
+
+// TODO long form length suppoort
+
+std::vector<unsigned char> create_integer(int input) {
+    std::vector<unsigned char> output;
+    output.push_back(0x02);
+    output.push_back(0x01);
+    output.push_back(input);
+    return output;
+}
+
+std::vector<unsigned char> create_octet_string(std::string input) {
+    std::vector<unsigned char> output;
+    output.push_back(0x04);
+    output.push_back(input.size());
+    for (char i : input) {
+        output.push_back(i);
+    }
+    return output;
+}
+
+std::vector<unsigned char> create_sequence(std::vector<std::vector<unsigned char>> input) {
+    std::vector<unsigned char> output;
+    output.push_back(0x30);
+    int total_size = 0;
+    for (std::vector<unsigned char> i : input) {
+        total_size += i.size();
+    }
+    output.push_back(total_size);
+    for (std::vector<unsigned char> i : input) {
+        for (unsigned char c : i) {
+            output.push_back(c);
+        }
+    }
+    return output;
+};
+
+std::vector<unsigned char> create_set(std::vector<std::vector<unsigned char>> input) {
+    std::vector<unsigned char> output;
+    output.push_back(0x31);
+    int total_size = 0;
+    for (std::vector<unsigned char> i : input) {
+        total_size += i.size();
+    }
+    output.push_back(total_size);
+    for (std::vector<unsigned char> i : input) {
+        for (unsigned char c : i) {
+            output.push_back(c);
+        }
+    }
+    return output;
+};
--- src/csv.h
+++ src/csv.h
@@ -5,6 +5,9 @@
 #include <string>
 #include <vector>
 
+// Name, mail, login are strings
+// Rows are vectors of strings
+// Data is vector of rows
 std::vector<std::vector<std::string> > read_csv(std::string filename);
 
 #endif  // CSV_H
