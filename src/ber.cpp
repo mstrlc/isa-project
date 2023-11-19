1274e75da8ba4ea0c15d85e72ec68bc86d044572
Matyáš Strelec <matyasstrelec@gmail.com>
2023-11-19 17:50:46 +0100

    Rewrite ber.cpp/h in OOP

--- src/ber.cpp
+++ src/ber.cpp
@@ -3,130 +3,296 @@
 #include <iomanip>
 #include <iostream>
 
-#include "ldap.h"
+class BERreader {
+    size_t iterator = 0;
+    ber_bytes bytes = ber_bytes();
 
-void print_hex(ber_bytes data) {
-    for (size_t i = 0; i < data.size(); i++) {
-        std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << (int)data[i] << " ";
+   public:
+    BERreader() {
+        this->bytes = ber_bytes();
+        this->iterator = 0;
     }
-    std::cout << std::endl;
-}
 
-ber_bytes create_length(size_t input) {
-    ber_bytes output;
+    BERreader(ber_bytes bytes) {
+        this->bytes = bytes;
+        this->iterator = 0;
+    }
 
-    if (input < 128) {
-        // short form
+    int read_integer() {
+        unsigned char tag = this->get_next_byte();
+        if (tag != BER_INTEGER) {
+            throw "error";
+        }
+        size_t length = this->get_length();
+        if (length > 4) {
+            throw "error";
+        }
+        int integer = 0;
+        for (size_t i = 0; i < length; i++) {
+            integer = integer << 8;
+            integer += this->get_next_byte();
+        }
+        return integer;
+    }
+
+    bool read_boolean() {
+        unsigned char tag = this->get_next_byte();
+        if (tag != BER_BOOLEAN) {
+            throw "error";
+        }
+        size_t length = this->get_length();
+        if (length > 1) {
+            throw "error";
+        }
+        bool boolean = this->get_next_byte();
+        return boolean;
+    }
+
+    std::string read_octet_string() {
+        unsigned char tag = this->get_next_byte();
+        if (tag != BER_OCTET_STRING) {
+            throw "error";
+        }
+        size_t length = this->get_length();
+        std::string octet_string = "";
+        for (size_t i = 0; i < length; i++) {
+            octet_string += this->get_next_byte();
+        }
+        return octet_string;
+    }
+
+    void read_sequence() {
+        unsigned char tag = this->get_next_byte();
+        if (tag != BER_SEQUENCE) {
+            throw "error";
+        }
+        this->get_length();
+    }
+
+    void read_set() {
+        unsigned char tag = this->get_next_byte();
+        if (tag != BER_SET) {
+            throw "error";
+        }
+        this->get_length();
+    }
+
+    int read_enumerated() {
+        unsigned char tag = this->get_next_byte();
+        if (tag != BER_ENUMERATED) {
+            throw "error";
+        }
+        size_t length = this->get_length();
+        if (length > 4) {
+            throw "error";
+        }
+        int enumerated = 0;
+        for (size_t i = 0; i < length; i++) {
+            enumerated = enumerated << 8;
+            enumerated += this->get_next_byte();
+        }
+        return enumerated;
+    }
+
+    unsigned char read_tag() {
+        unsigned char tag = this->get_next_byte();
+        get_length();
+        return tag;
+    }
+
+   private:
+    // Read next byte (unsigned char) from the bytes, increment iterator
+    unsigned char get_next_byte() {
+        if (this->iterator >= this->bytes.size()) {
+            throw "error";
+        } else {
+            return this->bytes[this->iterator++];
+        }
+    }
+
+    // Decode length (short form or long form)
+    size_t get_length() {
+        unsigned char length_byte = this->get_next_byte();
+        if (length_byte < 128) {
+            return length_byte;
+        } else {
+            size_t length = 0;
+            for (size_t i = 0; i < length_byte - 128; i++) {
+                length = length << 8;
+                length += this->get_next_byte();
+            }
+            return length;
+        }
+    }
+};
+
+class BERwriter {
+    ber_bytes bytes = ber_bytes();
+
+   public:
+    BERwriter() {}
+
+    ber_bytes build_ldapmessage(int message_id, ber_bytes protocolop_bytes) {
+        // MessageID
+        ber_bytes message_id_bytes = this->create_integer(message_id);
+
+        // ProtocolOp
+        ber_bytes protocol_op_bytes = protocolop_bytes;
+
+        // Controls
+        // TODO implement controls
+
+        // Put together the LDAPMessage
+        ber_bytes ldapmessage_bytes;
+
+        ldapmessage_bytes.push_back(BER_SEQUENCE);  // LDAPMessage
+        ber_bytes length_bytes = this->create_length(message_id_bytes.size() + protocol_op_bytes.size());
+        for (unsigned char c : length_bytes) {
+            ldapmessage_bytes.push_back(c);
+        }
+        ldapmessage_bytes.insert(ldapmessage_bytes.end(), message_id_bytes.begin(), message_id_bytes.end());
+        ldapmessage_bytes.insert(ldapmessage_bytes.end(), protocol_op_bytes.begin(), protocol_op_bytes.end());
+
+        return ldapmessage_bytes;
+    }
+
+    ber_bytes create_length(size_t input) {
+        ber_bytes output;
+
+        if (input < 128) {
+            // short form
+            output.push_back(input);
+        } else {
+            // long form
+            int bytes_needed = 0;
+            size_t temp = input;
+
+            while (temp > 0) {
+                temp >>= 8;
+                bytes_needed++;
+            }
+
+            // length of the length bytes
+            output.push_back(static_cast<unsigned char>(0x80 | bytes_needed));
+
+            // actual length bytes
+            for (int i = bytes_needed - 1; i >= 0; i--) {
+                output.push_back(static_cast<unsigned char>((input >> (8 * i)) & 0xFF));
+            }
+        }
+        return output;
+    }
+
+    ber_bytes create_integer(int input) {
+        ber_bytes output;
+        output.push_back(BER_INTEGER);
+        ber_bytes length = create_length(1);
+        for (unsigned char c : length) {
+            output.push_back(c);
+        }
         output.push_back(input);
-    } else {
-        // long form
-        int bytes_needed = 0;
-        size_t temp = input;
-
-        while (temp > 0) {
-            temp >>= 8;
-            bytes_needed++;
-        }
-
-        // length of the length bytes
-        output.push_back(static_cast<unsigned char>(0x80 | bytes_needed));
-
-        // actual length bytes
-        for (int i = bytes_needed - 1; i >= 0; i--) {
-            output.push_back(static_cast<unsigned char>((input >> (8 * i)) & 0xFF));
-        }
+        return output;
     }
-    return output;
-}
 
-ber_bytes create_integer(int input) {
-    ber_bytes output;
-    output.push_back(BER_INTEGER);
-    ber_bytes length = create_length(1);
-    for (unsigned char c : length) {
-        output.push_back(c);
-    }
-    output.push_back(input);
-    return output;
-}
-
-ber_bytes create_octet_string(std::string input) {
-    ber_bytes output;
-    output.push_back(BER_OCTET_STRING);
-    ber_bytes length = create_length(input.size());
-    for (unsigned char c : length) {
-        output.push_back(c);
-    }
-    for (char i : input) {
-        output.push_back(i);
-    }
-    return output;
-}
-
-ber_bytes create_sequence(std::vector<ber_bytes> input) {
-    ber_bytes output;
-    output.push_back(BER_SEQUENCE);
-    int total_size = 0;
-    for (ber_bytes i : input) {
-        total_size += i.size();
-    }
-    ber_bytes length = create_length(total_size);
-    for (unsigned char c : length) {
-        output.push_back(c);
-    }
-    for (ber_bytes i : input) {
-        for (unsigned char c : i) {
+    ber_bytes create_octet_string(std::string input) {
+        ber_bytes output;
+        output.push_back(BER_OCTET_STRING);
+        ber_bytes length = create_length(input.size());
+        for (unsigned char c : length) {
             output.push_back(c);
         }
+        for (char i : input) {
+            output.push_back(i);
+        }
+        return output;
     }
-    return output;
-};
 
-ber_bytes create_sequence(std::vector<ber_bytes> input, unsigned char tag) {
-    ber_bytes output;
-    output.push_back(tag);
-    int total_size = 0;
-    for (ber_bytes i : input) {
-        total_size += i.size();
-    }
-    ber_bytes length = create_length(total_size);
-    for (unsigned char c : length) {
-        output.push_back(c);
-    }
-    for (ber_bytes i : input) {
-        for (unsigned char c : i) {
+    ber_bytes create_sequence(std::vector<ber_bytes> input) {
+        ber_bytes output;
+        output.push_back(BER_SEQUENCE);
+        int total_size = 0;
+        for (ber_bytes i : input) {
+            total_size += i.size();
+        }
+        ber_bytes length = create_length(total_size);
+        for (unsigned char c : length) {
             output.push_back(c);
         }
-    }
-    return output;
-};
+        for (ber_bytes i : input) {
+            for (unsigned char c : i) {
+                output.push_back(c);
+            }
+        }
+        return output;
+    };
 
-ber_bytes create_set(std::vector<ber_bytes> input) {
-    ber_bytes output;
-    output.push_back(BER_SET);
-    int total_size = 0;
-    for (ber_bytes i : input) {
-        total_size += i.size();
-    }
-    ber_bytes length = create_length(total_size);
-    for (unsigned char c : length) {
-        output.push_back(c);
-    }
-    for (ber_bytes i : input) {
-        for (unsigned char c : i) {
+    ber_bytes create_sequence(std::vector<ber_bytes> input, unsigned char tag) {
+        ber_bytes output;
+        output.push_back(tag);
+        int total_size = 0;
+        for (ber_bytes i : input) {
+            total_size += i.size();
+        }
+        ber_bytes length = create_length(total_size);
+        for (unsigned char c : length) {
             output.push_back(c);
         }
-    }
-    return output;
-};
+        for (ber_bytes i : input) {
+            for (unsigned char c : i) {
+                output.push_back(c);
+            }
+        }
+        return output;
+    };
 
-ber_bytes create_enumerated(int input) {
-    ber_bytes output;
-    output.push_back(BER_ENUMERATED);
-    ber_bytes length = create_length(1);
-    for (unsigned char c : length) {
-        output.push_back(c);
+    ber_bytes create_set(std::vector<ber_bytes> input) {
+        ber_bytes output;
+        output.push_back(BER_SET);
+        int total_size = 0;
+        for (ber_bytes i : input) {
+            total_size += i.size();
+        }
+        ber_bytes length = create_length(total_size);
+        for (unsigned char c : length) {
+            output.push_back(c);
+        }
+        for (ber_bytes i : input) {
+            for (unsigned char c : i) {
+                output.push_back(c);
+            }
+        }
+        return output;
+    };
+
+    ber_bytes create_enumerated(int input) {
+        ber_bytes output;
+        output.push_back(BER_ENUMERATED);
+        ber_bytes length = create_length(1);
+        for (unsigned char c : length) {
+            output.push_back(c);
+        }
+        output.push_back(input);
+        return output;
     }
-    output.push_back(input);
-    return output;
-}
+
+    ber_bytes build_partialattributelist(std::vector<std::tuple<std::string, std::string>> attributes) {
+        std::vector<ber_bytes> partial_attributes_list;
+
+        for (std::tuple<std::string, std::string> attribute : attributes) {
+            // Type
+            ber_bytes type_bytes = create_octet_string(std::get<0>(attribute));
+
+            // Vals
+            ber_bytes vals_bytes = create_set({create_octet_string(std::get<1>(attribute))});
+
+            // Put together the PartialAttribute
+            ber_bytes partial_attribute_bytes = create_sequence({type_bytes, vals_bytes});
+
+            partial_attributes_list.push_back(partial_attribute_bytes);
+        }
+
+        // Put together the PartialAttributeList
+        ber_bytes partial_attribute_list_bytes = create_sequence(partial_attributes_list);
+        return partial_attribute_list_bytes;
+    }
+};
--- src/ber.h
+++ src/ber.h
@@ -1,26 +1,16 @@
-#ifndef BER_H
-#define BER_H
+#pragma once
 
 #include <string>
+#include <tuple>
 #include <vector>
 
-#define ber_bytes std::vector<unsigned char>
+// Constants
+const unsigned char BER_INTEGER = 0x02;
+const unsigned char BER_BOOLEAN = 0x01;
+const unsigned char BER_OCTET_STRING = 0x04;
+const unsigned char BER_SEQUENCE = 0x30;
+const unsigned char BER_SET = 0x31;
+const unsigned char BER_ENUMERATED = 0x0A;
 
-ber_bytes create_length(size_t input);
-
-void print_hex(ber_bytes data);
-
-ber_bytes create_integer(int input);
-
-ber_bytes create_octet_string(std::string input);
-
-// For default sequence tag
-ber_bytes create_sequence(std::vector<ber_bytes> input);
-// For specific application tags
-ber_bytes create_sequence(std::vector<ber_bytes> input, unsigned char tag);
-
-ber_bytes create_set(std::vector<ber_bytes> input);
-
-ber_bytes create_enumerated(int input);
-
-#endif  // BER_H
+// Type Definitions
+using ber_bytes = std::vector<unsigned char>;
