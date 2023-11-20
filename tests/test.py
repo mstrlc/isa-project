57083d31e9b01eb9169fbfa5d44ccae5240ec916
Matyáš Strelec <matyasstrelec@gmail.com>
2023-11-20 23:03:59 +0100

    Commit testing files

--- tests/test.py
+++ tests/test.py
@@ -6,37 +6,42 @@ if len(sys.argv) > 1:
     host = sys.argv[1]
     port = sys.argv[2]
 
-passed = True
+passed = 0
+failed = 0
 
 # Open file
 with open('testing.txt', 'r') as f:
     for line in f.readlines():
         if(line.startswith('--')):
             print(line)
-        if(line.startswith('"')):
+        if(line.startswith('(')):
             filter = line.split('#')[0].strip()
             expected = line.split('#')[1].strip()
             print("Filter: " + filter)
             print("Result: " + expected)
-            command = "ldapsearch -v -h " + host + " -p " + port + " -x " + filter
+            command = "ldapsearch -v -h " + host + " -p " + port + " -x \"" + filter + "\""
             print(command)
             run = subprocess.run(command, shell=True, capture_output=True)
-            output = run.stdout.decode('utf-8')
+            output = run.stdout.decode('utf-8').split('\n')
             for line in output:
-                if(line.startswith('# numReponses')):
-                    responses = int(line.split(':')[1])
-                    expected_responses = int(expected+1)
+                if("numResponses" in line):
+                    responses = int(line.split(':')[1])-1
+                    expected_responses = int(expected)
                     break
             if(responses == expected_responses):
                 print("Test passed!")
+                passed += 1
             else:
                 passed = False
-                print("Test failed!")
-                print("Expected: " + str(expected_responses))
+                print("\033[91mTest failed!\033[0m")
+                print("Expected: " + str(expected))
                 print("Got: " + str(responses))
+                failed += 1
 
-if(passed):
+if(failed == 0):
     print("All tests passed!")
+    print("Passed: " + str(passed))
 else:
     print("Some tests failed!")
+    print("Failed: " + str(failed))
     sys.exit(1)
