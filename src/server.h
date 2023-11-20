f87c453e66d0c6d17e94091f728d33b8ad703d6f
Matyáš Strelec <matyasstrelec@gmail.com>
2023-11-20 15:07:35 +0100

    Fix syntax

--- src/server.h
+++ src/server.h
@@ -1,6 +1,6 @@
 /*
  * server.h
- * 2023-11-20
+ * 2023-11-20t
  *
  * ISA project 2023
  * LDAP server
@@ -11,5 +11,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
 
-int server(int port, std::vector<std::vector<std::string> > data);
+int server(int port, std::vector <std::vector <std::string> > data);
